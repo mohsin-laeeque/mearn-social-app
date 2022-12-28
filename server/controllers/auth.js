881460f2834426d258eb626428b90561f3b6de5e
Mohsin <as.mohsin.dev@gmail.com>
2022-12-28 15:05:49 +0500

    clean

---
+++ server/controllers/auth.js
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+}
